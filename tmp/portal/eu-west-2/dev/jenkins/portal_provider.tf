136b60c4455a76145299921e178ba5d1836e3718
Siddu Angadi <siddu.angadi@gmail.com>
2023-09-06 23:23:12 +0100

    deleted tmp files

--- tmp/portal/eu-west-2/dev/jenkins/portal_provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region = var.dx_aws_region
-  default_tags {
-    tags = local.tags
-  }
-}
--- tmp/portal/eu-west-2/dev/jenkins/variable.tf
+++
@@ -1,5 +0,0 @@
-variable "instance_type" {
-  type = string
-  description = "instance_type for ec2"
-  default = "t2.micro"
-}
