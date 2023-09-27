54b767a18ebe082ce8284aa5f2ff203281c90fcc
Siddu Angadi <siddu.angadi@gmail.com>
2023-09-27 09:25:26 +0100

    updated backend

--- templates/portal/bakend.tf
+++ templates/portal/bakend.tf
@@ -1,7 +1,9 @@
-backend "s3" {
+terraform {
+  backend "s3" {
     bucket         	   = "%business_unit%-%env_name%-%aws_region%-s3-tfstate"
     key                = "%business_unit%/%env_name%/%aws_region%/terraform.tfstate"
     region         	   = "%aws_region%"
     encrypt        	   = true
     dynamodb_table = "%business_unit%-%env_name%-%aws_region%-dynamodb-tfstate"
   }
+}
