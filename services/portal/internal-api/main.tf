2bcbc9ac9eef289a4699e913386243689897a4a0
Siddu Angadi <siddu.angadi@gmail.com>
2023-09-27 09:49:42 +0100

    updated internal api

--- services/portal/internal-api/main.tf
+++ services/portal/internal-api/main.tf
@@ -4,9 +4,10 @@ module "ec2_cluster" {
 
   name           = "${var.dx_prefix_resource_name}-ec2"
   instance_count = "%instance_count%"
-
+  subnet_ids = split(var.subnet_ids, ",")
   ami           = local.ubuntu_ami_id
   instance_type = var.instance_type
+
   tags = {
     name = "${var.dx_prefix_resource_name}-ec2"
   }
--- services/portal/internal-api/variable.tf
+++ services/portal/internal-api/variable.tf
@@ -3,3 +3,8 @@ variable "instance_type" {
   description = "instance_type for ec2"
   default     = "%instance_type%"
 }
+
+variable "subnet_ids" {
+  description = "instance_type for ec2"
+  default     = "%subnet%"
+}
